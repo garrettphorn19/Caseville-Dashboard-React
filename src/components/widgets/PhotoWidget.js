2208294db298b93590a14a42019503f8e7866eea
Garrett Horn <garrett@sbfmail.com>
2023-06-16 17:25:23 -0400

    Photo Widget Mock

--- src/components/widgets/PhotoWidget.js
+++ src/components/widgets/PhotoWidget.js
@@ -2,7 +2,11 @@ import React from "react"
 import styled from "styled-components"
 
 export default function PhotoWidget() {
-  return <Widget></Widget>
+  return (
+    <Widget>
+      <Image src="/images/carousel/Image01.jpg" />
+    </Widget>
+  )
 }
 
 const Widget = styled.div`
@@ -12,9 +16,16 @@ const Widget = styled.div`
   left: 540px;
   top: 290px;
 
-  background: #ffffff;
+  background: "#FFFFFF";
   box-shadow: inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
   filter: drop-shadow(0px 1px 3px rgba(0, 0, 0, 0.1))
     drop-shadow(0px 20px 40px rgba(23, 0, 102, 0.2));
   border-radius: 25px;
+
+  overflow: hidden;
+`
+const Image = styled.img`
+  position: relative;
+  width: 100%;
+  top: -50px;
 `
